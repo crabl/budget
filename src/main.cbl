2ac053e7f582059111504a3b8cacd1e48f1e597d
Chris Rabl <rabl.christopher@gmail.com>
2016-07-14 23:10:03 -0600

    Engage disco flux capacitors

--- src/main.cbl
+++ src/main.cbl
@@ -1,5 +1,6 @@
 IDENTIFICATION DIVISION.
 	PROGRAM-ID. MAIN.
+	AUTHOR. CRABL.
 
 DATA DIVISION.
 	WORKING-STORAGE SECTION.
@@ -8,28 +9,32 @@ DATA DIVISION.
 				03 Principal PIC 9(5).
 				03 NumberOfYears PIC 9(4).
 				03 InterestRate PIC 9(4).
+				03 CalculationMethod PIC X(2).
 			02 Outputs.
-			 	03 SimpleInterest PIC 99999.99.
-				03 CompoundInterest PIC 99999.99.
+			 	03 SimpleInterest PIC 999999.99.
+				03 CompoundInterest PIC 999999.99.
 
 PROCEDURE DIVISION.
-	DISPLAY "ENTER PRINCIPAL" .
+	DISPLAY "Principal Amount: $" WITH NO ADVANCING.
 	ACCEPT Principal.
 
-	DISPLAY "ENTER LOAN TERM (YR) ".
+	DISPLAY "Loan Term (years): " WITH NO ADVANCING.
 	ACCEPT NumberOfYears.
 
-	DISPLAY "ENTER INTEREST RATE".
+	DISPLAY "Interest Rate (%): " WITH NO ADVANCING.
 	ACCEPT InterestRate.
 
-	DISPLAY "PRINCIPAL AMOUNT = ", Principal.
-	DISPLAY "NUMBER OF YEARS = ", NumberOfYears .
-	DISPLAY "INTEREST RATE = ", InterestRate.
+	DISPLAY "Calculation Method (SI/CO): " WITH NO ADVANCING.
+	ACCEPT CalculationMethod.
 
-	COMPUTE SimpleInterest = Principal + ((Principal * NumberOfYears * InterestRate) / 100).
-	DISPLAY "SIMPLE INTREST IS ", SimpleInterest.
-
-	COMPUTE CompoundInterest = Principal * (1 + (InterestRate / 100)) ** NumberOfYears.
-	DISPLAY "COMPOUND INTREST IS ", CompoundInterest.
-
+	EVALUATE CalculationMethod
+		WHEN "SI"
+			COMPUTE SimpleInterest = Principal + (Principal * NumberOfYears * (InterestRate / 100))
+			DISPLAY "SIMPLE INTREST IS ", SimpleInterest
+		WHEN "CO"
+			COMPUTE CompoundInterest = Principal * (1 + (InterestRate / 100)) ** NumberOfYears
+			DISPLAY "COMPOUND INTREST IS ", CompoundInterest
+		WHEN OTHER
+			DISPLAY "Invalid Calculation Method (must be either SI or CO)"
+	END-EVALUATE
 	STOP RUN.
