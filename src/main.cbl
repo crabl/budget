a511aebdee657cd84d1a5c29c28a213dd0566e0a
Chris Rabl <rabl.christopher@gmail.com>
2016-07-14 22:48:31 -0600

    updated interest calculation example

--- src/main.cbl
+++ src/main.cbl
@@ -2,7 +2,34 @@ IDENTIFICATION DIVISION.
 	PROGRAM-ID. MAIN.
 
 DATA DIVISION.
+	WORKING-STORAGE SECTION.
+		01 InterestCalculation.
+			02 Inputs.
+				03 Principal PIC 9(5).
+				03 NumberOfYears PIC 9(4).
+				03 InterestRate PIC 9(4).
+			02 Outputs.
+			 	03 SimpleInterest PIC 99999.99.
+				03 CompoundInterest PIC 99999.99.
 
 PROCEDURE DIVISION.
-	DISPLAY "Hello, world!".
+	DISPLAY "ENTER PRINCIPAL" .
+	ACCEPT Principal.
+
+	DISPLAY "ENTER LOAN TERM (YR) ".
+	ACCEPT NumberOfYears.
+
+	DISPLAY "ENTER INTEREST RATE".
+	ACCEPT InterestRate.
+
+	DISPLAY "PRINCIPAL AMOUNT = ", Principal.
+	DISPLAY "NUMBER OF YEARS = ", NumberOfYears .
+	DISPLAY "INTEREST RATE = ", InterestRate.
+
+	COMPUTE SimpleInterest = Principal + ((Principal * NumberOfYears * InterestRate) / 100).
+	DISPLAY "SIMPLE INTREST IS ", SimpleInterest.
+
+	COMPUTE CompoundInterest = Principal * (1 + (InterestRate / 100)) ** NumberOfYears.
+	DISPLAY "COMPOUND INTREST IS ", CompoundInterest.
+
 	STOP RUN.
